ab649c02f44241c4781e886eb116880708ec749c
Dmitry Moskalev <moskalev199618120@gmail.com>
2024-07-30 01:36:20 +0300

    add CRUD to Client model

--- mailing/urls.py
+++ mailing/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 
 from mailing.apps import MailingConfig
+from mailing.views import index, ClientListView, ClientCreateView, ClientDetailView, ClientUpdateView, ClientDeleteView
 
 app_name = MailingConfig.name
 
-urlpatterns = []
+urlpatterns = [
+    path('', index, name='index'),
+    path('client_list/', ClientListView.as_view(), name='client_list'),
+    path('client_create/', ClientCreateView.as_view(), name='client_create'),
+    path('client_detail/<int:pk>', ClientDetailView.as_view(), name='client_detail'),
+    path('client_update/<int:pk>', ClientUpdateView.as_view(), name='client_update'),
+    path('client_delete/<int:pk>', ClientDeleteView.as_view(), name='client_delete'),
+]
--- mailing/views.py
+++ mailing/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DetailView, ListView, UpdateView, DeleteView
 
-# Create your views here.
+from mailing.models import Client
+
+
+def index(request):
+    return render(request, 'mailing/index.html')
+
+
+class ClientListView(ListView):
+    model = Client
+
+
+class ClientDetailView(DetailView):
+    model = Client
+
+
+class ClientCreateView(CreateView):
+    model = Client
+    fields = ['name', 'email', 'comment']
+    success_url = reverse_lazy('mailing:clients_list')
+
+
+class ClientUpdateView(UpdateView):
+    model = Client
+    fields = ['name', 'email', 'comment']
+    success_url = reverse_lazy('mailing:clients_list')
+
+
+class ClientDeleteView(DeleteView):
+    model = Client
+    success_url = reverse_lazy('mailing:clients_list')
