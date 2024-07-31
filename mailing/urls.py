7694a471b4d5934dcebcf523785352e6d3e023bb
Dmitry Moskalev <moskalev199618120@gmail.com>
2024-07-31 22:59:32 +0300

    add new controller message (in views and urls), add pages for message and fix some bugs

--- mailing/urls.py
+++ mailing/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from mailing.apps import MailingConfig
-from mailing.views import index, ClientListView, ClientCreateView, ClientDetailView, ClientUpdateView, ClientDeleteView
+from mailing.views import index, ClientListView, ClientCreateView, ClientDetailView, ClientUpdateView, ClientDeleteView, \
+    MessageListView, MessageCreateView, MessageDetailView, MessageUpdateView, MessageDeleteView
 
 app_name = MailingConfig.name
 
@@ -12,4 +13,9 @@ urlpatterns = [
     path('client_detail/<int:pk>', ClientDetailView.as_view(), name='client_detail'),
     path('client_update/<int:pk>', ClientUpdateView.as_view(), name='client_update'),
     path('client_delete/<int:pk>', ClientDeleteView.as_view(), name='client_delete'),
+    path('message_list/', MessageListView.as_view(), name='message_list'),
+    path('message_create/', MessageCreateView.as_view(), name='message_create'),
+    path('message_detail/<int:pk>', MessageDetailView.as_view(), name='message_detail'),
+    path('message_update/<int:pk>', MessageUpdateView.as_view(), name='message_update'),
+    path('message_delete/<int:pk>', MessageDeleteView.as_view(), name='message_delete'),
 ]
--- mailing/views.py
+++ mailing/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, DetailView, ListView, UpdateView, DeleteView
 
-from mailing.models import Client
+from mailing.models import Client, Message
 
 
 def index(request):
@@ -28,7 +28,39 @@ class ClientUpdateView(UpdateView):
     fields = ['name', 'email', 'comment']
     success_url = reverse_lazy('mailing:client_list')
 
+    def get_success_url(self):
+        return reverse('mailing:client_detail', args=[self.kwargs.get('pk')])
+
 
 class ClientDeleteView(DeleteView):
     model = Client
     success_url = reverse_lazy('mailing:client_list')
+
+
+class MessageListView(ListView):
+    model = Message
+
+
+class MessageDetailView(DetailView):
+    model = Message
+
+
+class MessageCreateView(CreateView):
+    model = Message
+    fields = ['title', 'text',]
+    success_url = reverse_lazy('mailing:message_list')
+
+
+class MessageUpdateView(UpdateView):
+    model = Message
+    fields = ['title', 'text',]
+    success_url = reverse_lazy('mailing:message_list')
+
+    def get_success_url(self):
+        return reverse('mailing:message_detail', args=[self.kwargs.get('pk')])
+
+
+class MessageDeleteView(DeleteView):
+    model = Message
+    success_url = reverse_lazy('mailing:message_list')
+
